a9b714c1f283d3f82f480c1b83ac3b9acd952504
muhammed-jasim <muhammedjasim1000@gmail.com>
2024-02-04 11:21:54 +0530

    adding-database

--- Flash_Fiesta/settings.py
+++ Flash_Fiesta/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Flash_Fiesta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flash_fiesta',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '12jasim12',
     }
 }
 

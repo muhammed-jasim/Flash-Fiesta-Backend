122f7e42520ee593080a3811ae83e140ad024ece
muhammed-jasim <muhammedjasim1000@gmail.com>
2024-07-09 21:55:04 +0530

    home

--- Flash_Fiesta/settings.py
+++ Flash_Fiesta/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'Flash_Fiesta.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
